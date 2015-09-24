1a66ec8eb1994000d0673076aee426a523762c56
AJIT <ajit.palakkad@gmail.com>
2015-09-24 09:14:06 +0530

    hfh

--- Gruntfile.js
+++ Gruntfile.js
@@ -402,7 +402,7 @@ module.exports = function (grunt) {
       },
       heroku: {
         options: {
-          remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+          remote: 'git@heroku.com:serene-badlands-4567.git',
           branch: 'master',
           tag: pkg.version
         }
